02c570f47d65ff59205cbd6568db8c17f89003bf
WeeenZh <wenzhengtao@foxmail.com>
2024-07-18 14:44:54 -0600

    bpca in r with ard, time

--- script/r/bpca.r
+++ script/r/bpca.r
@@ -34,10 +34,111 @@ eigenvalplotslg.bPCA <- function(bPCA.fitted, data)
 } 
 
 
-simulate2 <- function(psi = 1) {
+sim.bPCA_ard <- function(data, 
+                     Q, 
+                     mu.prior, 
+                     mu.prior.cov, 
+                     a_alpha, 
+                     b_alpha, 
+                     a_tau, 
+                     b_tau, 
+                     n.chains = 3, 
+                     n.iter = 5000, 
+                     n.burnin = 4500) {
+  # requirements
+  require(R2jags)
+  require(MASS)
+  require(Matrix)
+  require(coda)
+  
+  # dataset dimensions
+  N <- nrow(data)
+  V <- ncol(data)
+  
+  # defaults for priors
+  if (missing(mu.prior)) mu.prior <- rep(0, V)
+  if (missing(mu.prior.cov)) mu.prior.cov <- as.matrix(Diagonal(V, 1000))
+  if (missing(a_alpha)) a_alpha <- 1e-3
+  if (missing(b_alpha)) b_alpha <- 1e-3
+  if (missing(a_tau)) a_tau <- 1e-3
+  if (missing(b_tau)) b_tau <- 1e-3
+  
+  # makes precisions from covariances
+  mu.prior.prec <- solve(mu.prior.cov)
+  
+  # puts data into list
+  listdata <- list(Y = as.matrix(data), 
+                   N = N,
+                   V = V, 
+                   Q = Q,
+                   mu.prior = mu.prior, 
+                   mu.prior.prec = mu.prior.prec,
+                   a_alpha = a_alpha,
+                   b_alpha = b_alpha,
+                   a_tau = a_tau,
+                   b_tau = b_tau)
+  
+  # defines the model in JAGS language
+  cat("
+  model {
+    # Priors for the mean vector
+    mu[1:V] ~ dmnorm(mu.prior[], mu.prior.prec[,])
+    
+    # Priors for the weight matrix W
+    for (i in 1:V) {
+      for (j in 1:Q) {
+        W[i, j] ~ dnorm(0, alpha[j])
+      }
+    }
+
+    # Priors for precision parameters alpha
+    for (j in 1:Q) {
+      alpha[j] ~ dgamma(a_alpha, b_alpha)
+    }
+
+    # Prior for noise precision tau
+    tau ~ dgamma(a_tau, b_tau)
+    
+    # Priors for latent variables X
+    for (i in 1:N) {
+      for (j in 1:Q) {
+        X[i, j] ~ dnorm(0, 1)
+      }
+    }
+
+    # Define identity matrix
+    for (i in 1:V) {
+      for (j in 1:V) {
+        I[i, j] <- equals(i, j)
+      }
+    }
+    
+    # Likelihood
+    for (i in 1:N) {
+      mu_pred[i, 1:V] <- mu[] + W[,] %*% X[i,]
+      Y[i, 1:V] ~ dmnorm(mu_pred[i,], tau * I[,])
+    }
+  }
+  ", file = "PCA_hierarchical.bugs")
+  
+  # jags model to estimate covariance matrix distribution
+  pcabay <- jags(data = listdata,
+                 model.file = "PCA_hierarchical.bugs",
+                #  parameters.to.save = c("mu", "W", "alpha", "tau"),
+                 parameters.to.save = c("alpha", "tau"),
+                 n.chains = n.chains,
+                 n.iter = n.iter,
+                 n.burnin = n.burnin, 
+                 DIC = FALSE)
+  
+  return(pcabay)
+}
+
+
+
+
+simulate2 <- function(psi = 1, N = 100, P = 10) {
   psi_inv <- 1 / psi
-  N <- 100
-  P <- 10
   cov <- diag(c(5, 4, 3, 2, rep(psi_inv, P - 4)))
   
   # Generate a random orthogonal matrix U
@@ -53,8 +154,10 @@ simulate2 <- function(psi = 1) {
 }
 
 set.seed(123)
-var_noise = 0.1
-data <- simulate2(psi = 1 / var_noise)
+var_noise <- 0.01
+N <- 100
+P <- 10
+data <- simulate2(psi = 1 / var_noise, N = N, P = P)
 
 covmat_prior <- as.matrix(Diagonal(ncol(data), 1))
 covmat_prior_DF <- ncol(data)
@@ -70,3 +173,31 @@ result <- sim.bPCA(data = data,
 eigenval_results <- eigenvalplotslg.bPCA(result, data)
 # print(eigenval_results)
 # biplots.bPCA(result, data)
+
+
+start_time <- Sys.time()
+
+# # Run the function with wishart prior
+# result <- sim.bPCA(data = data,
+#                    covmat.prior = covmat_prior, 
+#                    covmat.prior.DF = covmat_prior_DF, 
+#                    n.chains = 3,
+#                    n.iter = 200,
+#                 #    n.iter = 1200,
+#                    n.burnin = 100)
+# Run the model with hierarchical prior
+bpca.fit <- sim.bPCA_ard(data = data, Q = P-1, n.chains = 1, n.iter = 1200, n.burnin = 100)
+# plot(-sort(log10(colMeans(bpca_fit$BUGSoutput$sims.matrix[, -ncol(bpca_fit$BUGSoutput$sims.matrix)]))),
+#     type = "b", pch = 19, col = "blue",
+#     xlab = "Index", ylab = "Log10(Column Means)",
+#     main = "Inverse Sorted Log10 of Column Means"
+# )
+
+# End timing
+end_time <- Sys.time()
+
+# Calculate the elapsed time
+elapsed_time <- end_time - start_time
+
+# Print the elapsed time
+print(elapsed_time)
